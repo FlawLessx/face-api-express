6ac4b6143092e3c5c4be47bcbec2f24ac3ea99ff
flawlessx <yanuarbimantoro@gmail.com>
2022-11-17 13:10:21 +0700

    fix: path

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
-const faceApiService = require("./faceApiService");
+const faceApiService = require("./faceapiService");
 
 const app = express();
 const port = process.env.PORT || 3000;
